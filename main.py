a6114015d0b9c7d8d82907c4e9aade8184bc5dce
Cal Paterson <cal@calpaterson.com>
2019-06-18 13:54:47 +0100

    Get as far as retrieving the video urls

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from os import environ
+import json
+import re
 
 from selenium import webdriver
 
 from selenium.webdriver.common.keys import Keys
 
-def handle_section(section):
+config_regex = re.compile("var config = (.*);")
+
+def handle_script(script):
+    script_text = script.get_attribute("innerHTML")
+    mobj = config_regex.search(script_text)
+    unparsed_json = mobj.groups()[0]
+    parsed_json = json.loads(unparsed_json)
+    files = parsed_json["request"]["files"]["progressive"]
+    print(json.dumps(files, indent=4))
+    breakpoint()
+
+
+def handle_episode(driver, episode):
+    episode.click()
+    player_frame = driver.find_element_by_css_selector("#vimeo_player > iframe")
+    driver.switch_to.frame(player_frame)
+    script = driver.find_element_by_css_selector(".vp-center > script")
+
+    handle_script(script)
+
+    driver.switch_to.default_content()
+
+
+def handle_section(driver, section):
     for episode in section.find_elements_by_css_selector(".Episode ._content"):
-        episode.click()
-        breakpoint()
+        handle_episode(driver, episode)
+
 
 def main():
     driver = webdriver.Firefox()
@@ -22,11 +47,12 @@ def main():
     password.send_keys(environ["HACW_PASSWORD"])
     password.send_keys(Keys.RETURN)
 
+    driver.implicitly_wait(3)
     sections = driver.find_elements_by_css_selector(".Section")
     for section in sections:
-        handle_section(section)
+        handle_section(driver, section)
 
-    breakpoint()
+    driver.quit()
 
 if __name__ == "__main__":
     main()
