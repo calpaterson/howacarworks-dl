9f2d96db7df803d384ccce0d8ee8e032721bec9e
Cal Paterson <cal@calpaterson.com>
2019-06-19 13:52:34 +0100

    Close by no cigar

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 from os import environ
+from sys import stdout
 import json
 import re
+import time
 
 from selenium import webdriver
 
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
 
 config_regex = re.compile("var config = (.*);")
 
-def handle_script(script):
+def handle_script(script: WebElement) -> str:
+    """Returns download url of highest quality version"""
     script_text = script.get_attribute("innerHTML")
     mobj = config_regex.search(script_text)
     unparsed_json = mobj.groups()[0]
     parsed_json = json.loads(unparsed_json)
     files = parsed_json["request"]["files"]["progressive"]
-    print(json.dumps(files, indent=4))
-    breakpoint()
+    top_quality = sorted(files, key=lambda f: f["height"], reverse=True)[1]["url"]
+    return top_quality
 
 
-def handle_episode(driver, episode):
+def handle_episode(driver, section_name, episode):
+    episode_name = episode.text.split("\n")[0]
     episode.click()
+    time.sleep(3)
+
     player_frame = driver.find_element_by_css_selector("#vimeo_player > iframe")
+
     driver.switch_to.frame(player_frame)
+    time.sleep(3)
     script = driver.find_element_by_css_selector(".vp-center > script")
 
-    handle_script(script)
+    url = handle_script(script)
+    stdout.write("%s\n  dir=%s\n  out=%s.mp4\n" % (url, section_name, episode_name))
 
     driver.switch_to.default_content()
 
 
 def handle_section(driver, section):
+    section_name = section.find_element_by_css_selector("h2").text
     for episode in section.find_elements_by_css_selector(".Episode ._content"):
-        handle_episode(driver, episode)
+        handle_episode(driver, section_name, episode)
 
 
 def main():
